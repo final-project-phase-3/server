424e2c981f65ef4bb1291f293af54c2b9150bcba
kevin tan <kevintan203@gmail.com>
2020-03-21 20:10:27 +0700

    add search feature

--- foodService/controllers/foodController.js
+++ foodService/controllers/foodController.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const baseUrl = 'https://api.spoonacular.com/recipes/'
 
 class FoodController{
   static findRecipe(req,res,next) {
@@ -7,10 +8,9 @@ class FoodController{
     const promiseNutritions = []
     console.log(req.body)
     const combineIngridients = req.body.data.join(",+")
-    console.log(combineIngridients)
+    // console.log(combineIngridients)
     axios({
-      url: `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${combineIngridients}&number=2&apiKey=${process.env.API_KEY}`,
-      // url: `https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=${process.env.API_KEY}`,
+      url: `${baseUrl}findByIngredients?ingredients=${combineIngridients}&number=2&apiKey=${process.env.API_KEY}`,
       method: 'get'
     })
       .then(resp=>{
@@ -19,13 +19,13 @@ class FoodController{
           combinedRecipesData.push(resp.data[i])
           promiseRecipes.push(
             axios({
-              url: `https://api.spoonacular.com/recipes/${resp.data[i].id}/analyzedInstructions?apiKey=${process.env.API_KEY}`,
+              url: `${baseUrl}${resp.data[i].id}/analyzedInstructions?apiKey=${process.env.API_KEY}`,
               method: "get"
             })
           );
           promiseNutritions.push(
             axios({
-              url: `https://api.spoonacular.com/recipes/${resp.data[i].id}/information?includeNutrition=true&apiKey=${process.env.API_KEY}`,
+              url: `${baseUrl}${resp.data[i].id}/information?includeNutrition=true&apiKey=${process.env.API_KEY}`,
               method: 'get'
             })
           );
@@ -34,36 +34,89 @@ class FoodController{
       })
       .then(steprecipe => {
         console.log("masuk",steprecipe.length,combinedRecipesData.length)
-        // if(steprecipe.length === 0){
-        //   for (let i = 0; i < combinedRecipesData.length; i++) {
-        //     combinedRecipesData[i].cookingSteps = [];
-        //   }
-        // }else{
-          for (let i = 0; i < steprecipe.length; i++) {
-            console.log(steprecipe[i],"step")
-            if(steprecipe[i].data.length === 0){
-              combinedRecipesData[i].cookingSteps = [];
-            }else{
-              combinedRecipesData[i].cookingSteps = steprecipe[i].data[0].steps;
-            }
+        for (let i = 0; i < steprecipe.length; i++) {
+          // console.log(steprecipe[i],"step")
+          if(steprecipe[i].data.length === 0){
+            combinedRecipesData[i].cookingSteps = [];
+          }else{
+            combinedRecipesData[i].cookingSteps = steprecipe[i].data[0].steps;
           }
-        // }
-        console.log(combinedRecipesData,"withstep")
+        }
+        // console.log(combinedRecipesData,"withstep")
         return Promise.all(promiseNutritions)
       })
       .then(foodNutritions=>{
         for(let i = 0; i < foodNutritions.length; i++) {
-          console.log(foodNutritions[i],"nutri")
+          // console.log(foodNutritions[i],"nutri")
           combinedRecipesData[i].nutritions = foodNutritions[i].data.nutrition.nutrients
-          combinedRecipesData[i].servingTime = foodNutritions[i].data.readyInMinutes
+          combinedRecipesData[i].readyInMinutes = foodNutritions[i].data.readyInMinutes
         }
-        console.log(combinedRecipesData,"<<<result");
+        // console.log(combinedRecipesData,"<<<result");
         res.status(200).json({ payload: combinedRecipesData });
       })
       .catch(error => {
         console.log("error");
         next(error);
       });
+  }
+  static searchRecipe(req,res,next) {
+    console.log(req.body.data)
+    const combinedRecipesData = []
+    const promiseRecipes = []
+    const promiseNutritions = []
+    let query = req.body.data
+    axios({
+      // url: `https://api.spoonacular.com/recipes/search?query=cheese&number=2&apiKey=${process.env.API_KEY}`,
+      url: `${baseUrl}search?query=${query}&number=2&apiKey=${process.env.API_KEY}`,
+      method: 'get'
+    })
+      .then(recipes=>{
+        const shortAccess = recipes.data.results 
+        for(let i = 0; i < shortAccess.length; i++){
+          combinedRecipesData.push(shortAccess[i])
+          promiseRecipes.push(
+            axios({
+              url: `${baseUrl}${shortAccess[i].id}/analyzedInstructions?apiKey=${process.env.API_KEY}`,
+              method: "get"
+            })
+          );
+          promiseNutritions.push(
+            axios({
+              url: `${baseUrl}${shortAccess[i].id}/information?includeNutrition=true&apiKey=${process.env.API_KEY}`,
+              method: 'get'
+            })
+          );
+        }
+        return Promise.all(promiseRecipes)
+        console.log(combinedRecipesData)
+      })
+      .then(steprecipe => {
+        console.log("masuk",steprecipe.length,combinedRecipesData.length)
+        for (let i = 0; i < steprecipe.length; i++) {
+          console.log(steprecipe[i],"step")
+          if(steprecipe[i].data.length === 0){
+            combinedRecipesData[i].cookingSteps = [];
+          }else{
+            combinedRecipesData[i].cookingSteps = steprecipe[i].data[0].steps;
+          }
+        }
+        console.log(combinedRecipesData,"withstep")
+        return Promise.all(promiseNutritions)
+      })
+      .then(foodNutritions=>{
+        for(let i = 0; i < foodNutritions.length; i++) {
+          // console.log(foodNutritions[i].data.nutrition.ingredients,"from nutri")
+          // console.log(foodNutritions[i].data.extendedIngredients,"from extend")
+          combinedRecipesData[i].nutritions = foodNutritions[i].data.nutrition.nutrients
+          combinedRecipesData[i].ingredients = foodNutritions[i].data.extendedIngredients          
+        }
+        console.log(combinedRecipesData,"<<<result");
+        res.status(200).json({ payload: combinedRecipesData });
+      })
+      .catch(error=>{
+        console.log(error)
+        next(error)
+      })
   }
 }
 
--- foodService/routes/foodRoute.js
+++ foodService/routes/foodRoute.js
@@ -2,6 +2,7 @@ const foodRoute = require("express").Router();
 const foodController = require("../controllers/foodController");
 
 foodRoute.post('/recipe', foodController.findRecipe)
+foodRoute.post('/searchRecipe', foodController.searchRecipe)
 
 
 module.exports = foodRoute;
