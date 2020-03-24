61b606ba411cf67003a08cb0f5f7972ec168f89a
Amalya Adyanissa <amalya.adyanissa@example.com>
2020-03-24 19:39:16 +0700

    nambah test dan route register

--- foodService/controllers/userController.js
+++ foodService/controllers/userController.js
@@ -1,6 +1,6 @@
-const User = require('../models/User')
-const {generateToken} = require('../helper/jwt')
-const {OAuth2Client} = require('google-auth-library')
+const User = require("../models/User");
+const { generateToken } = require("../helper/jwt");
+const { OAuth2Client } = require("google-auth-library");
 
 class userController {
   /* istanbul ignore next */
@@ -48,29 +48,68 @@ class userController {
   }
 
   static getUser(req, res, next) {
-    console.log(req.payload)
+    console.log(req.payload);
     User.findById(req.payload.id)
       .then(user => {
-        console.log(user)
-        if(user){
+        console.log(user);
+        if (user) {
           res.status(200).json(user);
-        }else{
+        } else {
           throw {
             status: 404,
             message: "User Not Found"
-          }
+          };
         }
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
         next(err);
       });
   }
   /* istanbul ignore next */
-  static createTestToken(req,res,next){
-    const { id } = req.body
-    let token = generateToken({id}, process.env.JWT_SECRET);
-    res.status(200).json({token})
+  static createTestToken(req, res, next) {
+    const { id } = req.body;
+    let token = generateToken({ id }, process.env.JWT_SECRET);
+    res.status(200).json({ token });
+  }
+
+  static register(req, res, next) {
+    User.findOne({ email: req.body.email })
+      .then(found => {
+        if (found) {
+          throw { status: 400, message: "Email already registered" };
+        } else {
+          if (req.body.password) {
+            if (req.body.password.length < 6) {
+              throw {
+                status: 400,
+                message: "Minimal character for password is 6"
+              };
+            }
+          }
+          return User.create({
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            refrigerator: []
+          });
+        }
+      })
+      .then(userRegistered => {
+        const token = generateToken(
+          { id: userRegistered._id },
+          process.env.JWT_SECRET
+        );
+        const outputUser = {
+          username: userRegistered.username,
+          refrigerator: userRegistered.refrigerator,
+          email: userRegistered.email
+        };
+        res.status(201).json({ userRegistered: outputUser, token });
+      })
+      .catch(err => {
+        next(err);
+      });
   }
 }
 
--- foodService/routes/userRoute.js
+++ foodService/routes/userRoute.js
@@ -3,7 +3,8 @@ const userController = require("../controllers/userController");
 const authentication = require("../middlewares/authentication");
 
 userRoute.get("/", authentication, userController.getUser);
-userRoute.post('/login',userController.loginGoogle)
-userRoute.post("/createtoken",userController.createTestToken)
+userRoute.post("/login", userController.loginGoogle);
+userRoute.post("/createtoken", userController.createTestToken);
+userRoute.post("/register", userController.register);
 
 module.exports = userRoute;
