e0aeb495eca1f5513f8efbadf1bb5f7ff5b4e07a
jetly sandita <jetly27sandita@gmail.com>
2020-03-24 22:06:31 +0700

    merge

--- foodService/controllers/processImage.js
+++ foodService/controllers/processImage.js
@@ -1,5 +1,6 @@
 const axios = require("axios")
 const textApi = require('../services/aylien')
+const qs = require('qs')
 class ProcessImageController {
   static async uploadImage(req,res,next){
     
@@ -17,8 +18,7 @@ class ProcessImageController {
       }
       next(err)
     }
-    const fileName = imageUrl.substring(imageUrl.lastIndexOf('/')+1);
-    const sharpenImage = `https://coolkas.imgix.net/${fileName}?sharp=100?dpr=0.5`
+    const sharpenImage = `https://coolkas.imgix.net/${fileName}?sharp=100&vib=50&sat=25`
 
     const auth = {
       auth:{
@@ -27,7 +27,7 @@ class ProcessImageController {
       }
     }
 
-    const url = 'https://api.imagga.com/v2/tags?limit=5&&image_url='+encodeURIComponent(sharpenImage)
+    const url = 'https://api.imagga.com/v2/tags?limit=3&&image_url='+encodeURIComponent(sharpenImage)
     axios
       .get(url,auth)
       .then(({ data }) => {
@@ -36,32 +36,80 @@ class ProcessImageController {
         for (let i = 0; i < data.result.tags.length;i++){
           arrayTag.push(data.result.tags[i].tag.en)
         }
+        if(arrayTag.length === 0){
+          const err = {
+            status:400,
+            message:"imageUrl missing"
+          }
+          next(err)
+        }
         textApi.concepts({
           text:String(arrayTag),
           language:"en"
-        },(error,response) => {
+        },async (error,response) => {
           console.log(arrayTag)
           if (error === null) {
             let arrayKey = Object.keys(response.concepts)
-            let concept = arrayKey[0]
-            let surfaceForms = response.concepts[concept].surfaceForms.map(function(sf) {
-              return sf['string'];
-            });
-            textApi.hashtags({
-              text:String(arrayTag),
-              language:"en"
-            },((err,resp) => {
-              let tags = resp.hashtags.map(sf => {
-                return sf.slice(1)
+            console.log(arrayKey)
+            let arrayResult = []
+            for(let i = 0 ; i < arrayKey.length; i++){
+              console.log(response.concepts[arrayKey[i]] )
+              arrayResult = arrayResult.concat(response.concepts[arrayKey[i]].surfaceForms)
+            }
+            
+            // arrayResult.sort((a,b) => {
+            //   return b.score - a.score
+            // });
+            let tags = []
+            arrayResult.forEach(val => {
+              tags.push(val.string)
+            })
+            console.log(arrayResult)
+            try {
+              
+              console.log(tags)
+              const razorResult = await axios(
+                {
+                  url:"http://api.textrazor.com",
+                  method:"GET",
+                  headers:{
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'X-TextRazor-Key': process.env.RAZOR_KEY
+                  },
+                  data:qs.stringify({
+                    text: arrayTag.join(" "),
+                    extractors:"entities",
+                    languageOverride:"eng"
+                  })
+                }
+              )
+              const razorTemp = razorResult.data.response.entities || []
+              console.log(razorTemp)
+              let newtags = []
+              razorTemp.forEach(el => {
+                newtags.push(el.matchedText)
               })
               res.status(200).json({
                 imageUrl,
-                concept: concept.substring(concept.lastIndexOf('/')+1),
-                name: surfaceForms[0],
-                tags: tags,
+                // concept: concept.substring(concept.lastIndexOf('/')+1),
+                name: newtags[0] || '',
+                tags:newtags,
                 msg: "Success"
               })
-            }))
+            } catch (error) {
+                console.log(error)
+            }
+
+           
+            // textApi.hashtags({
+            //   text:String(arrayTag),
+            //   language:"en"
+            // },((err,resp) => {
+            //   let tags = resp.hashtags.map(sf => {
+            //     return sf.slice(1)
+            //   })
+              
+            // }))
           }else {
           /* istanbul ignore next */
             throw {
@@ -70,18 +118,9 @@ class ProcessImageController {
             }
           }
         })
-        // if((data.status.type === 'success' || data.result.tags.length === 0)){
-          //   res.json(data.result.tags)
-        //   // res.status(200).json({name: data.result.tags[0].tag.en})
-        // }else{
-          // }
-        })
-        .catch(err => {
-          /* istanbul ignore next */
-          throw {
-            status:400,
-            message:"can't read your image"
-          }
+      })
+      .catch(err => {
+        next(err)
       })
   }
 }
